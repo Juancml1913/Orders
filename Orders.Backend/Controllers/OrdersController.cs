b3a32cf4c507a113b8306867fcb2988abe240c0b
Juan Camilo Muñoz <juancml1913@gmail.com>
2025-08-25 13:55:07 -0500

    Order index Admin

--- Orders.Backend/Controllers/OrdersController.cs
+++ Orders.Backend/Controllers/OrdersController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Orders.Backend.Helpers;
+using Orders.Backend.UnitsOfWork.Implementations;
+using Orders.Backend.UnitsOfWork.Interfaces;
 using Orders.Shared.DTOs;
 
 namespace Orders.Backend.Controllers
@@ -12,10 +14,34 @@ namespace Orders.Backend.Controllers
     public class OrdersController : ControllerBase
     {
         private readonly IOrdersHelper _ordersHelper;
+        private readonly IOrdersUnitOfWork _ordersUnitOfWork;
 
-        public OrdersController(IOrdersHelper ordersHelper)
+        public OrdersController(IOrdersHelper ordersHelper, IOrdersUnitOfWork ordersUnitOfWork)
         {
             _ordersHelper=ordersHelper;
+            _ordersUnitOfWork=ordersUnitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync([FromQuery] PaginationDTO pagination)
+        {
+            var response = await _ordersUnitOfWork.GetAsync(User.Identity!.Name!, pagination);
+            if (response.WasSuccess)
+            {
+                return Ok(response.Result);
+            }
+            return BadRequest();
+        }
+
+        [HttpGet("totalPages")]
+        public async Task<IActionResult> GetPagesAsync([FromQuery] PaginationDTO pagination)
+        {
+            var response = await _ordersUnitOfWork.GetTotalPagesAsync(User.Identity!.Name!, pagination);
+            if (response.WasSuccess)
+            {
+                return Ok(response.Result);
+            }
+            return BadRequest(response.Message);
         }
 
         [HttpPost]
---
+++ Orders.Frontend/Pages/Cart/OrdersIndex.razor.cs
@@ -0,0 +1,90 @@
+using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Orders.Frontend.Repositories;
+using Orders.Shared.Entities;
+
+namespace Orders.Frontend.Pages.Cart
+{
+    [Authorize(Roles = "Admin")]
+    public partial class OrdersIndex
+    {
+        [Inject] private IRepository Repository { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+
+        private int currentPage = 1;
+        private int totalPages;
+
+        public List<Order>? Orders { get; set; }
+
+        [Parameter, SupplyParameterFromQuery] public int RecordsNumber { get; set; } = 10;
+        [Parameter, SupplyParameterFromQuery] public string Page { get; set; } = string.Empty;
+
+        protected async override Task OnInitializedAsync()
+        {
+            await LoadAsync();
+        }
+
+        private async Task SelectedPageAsync(int page)
+        {
+            currentPage = page;
+            await LoadAsync(page);
+        }
+
+        private async Task SelectedRecordsNumberAsync(int recordsNumber)
+        {
+            RecordsNumber = recordsNumber;
+            await LoadAsync();
+        }
+
+        private async Task LoadAsync(int page = 1)
+        {
+            if (!string.IsNullOrWhiteSpace(Page))
+            {
+                page = Convert.ToInt32(Page);
+            }
+            var ok = await LoadListAsync(page);
+            if (ok)
+            {
+                await LoadPagesAsync();
+            }
+        }
+
+        private void ValidateRecordsNumber(int recordsNumber)
+        {
+            if (recordsNumber == 0)
+            {
+                RecordsNumber = 10;
+            }
+        }
+
+        private async Task LoadPagesAsync()
+        {
+            var url = $"api/orders/totalpages?RecordsNumber={RecordsNumber}";
+            var responseHttp = await Repository.GetAsync<int>(url);
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+            totalPages = responseHttp.Response;
+        }
+
+        private async Task<bool> LoadListAsync(int page)
+        {
+            ValidateRecordsNumber(RecordsNumber);
+            var url = $"api/orders?page={page}&RecordsNumber={RecordsNumber}";
+            var responseHttp = await Repository.GetAsync<List<Order>>(url);
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return false;
+            }
+            Orders = responseHttp.Response;
+            return true;
+        }
+    }
+}
